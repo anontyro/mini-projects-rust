2dbb6a03d07c640d03e36a6650d9a58227e842b0
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-10-07 17:54:56 +0800

    building the basic logic to handle the different programs currently with if else

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 extern crate main_lib;
+use std::io;
 
 fn main() {
-    main_lib::test();
-    main_lib::temp_main::temp_convert::temp();
+    // main_lib::test();
+    user_select();
+}
+
+fn user_select() {
+    loop {
+        println!("Select a project from the list, or press x to exit" );
+
+        let mut user_input = String::new();
+
+        io::stdin().read_line(&mut user_input);
+
+        println!("user input: {}", user_input );
+
+        if user_input.trim() == "x" {
+            break;
+        } else if user_input.trim() == "1" {
+            main_lib::temp_main::temp_convert::temp();
+        }
+
+
+        
+    }
 }
--- src/projects/lib.rs
+++ src/projects/lib.rs
@@ -1,3 +1,4 @@
+#[path="./temp/temp_main.rs"]
 pub mod temp_main;
 
 pub fn test() {
